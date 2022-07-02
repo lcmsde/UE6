fbc94e87612ed916c77302a18822de5731c32b00
lcmsde <lcmsde@protonmail.com>
2022-07-02 14:58:41 +0200

    ende Samstag

--- src/Binaerbaum.java
+++ src/Binaerbaum.java
@@ -1,2 +1,116 @@
-package PACKAGE_NAME;public class Binaerbaum {
+public class Binaerbaum<T> {
+    BaumEl wurzel;
+
+    class BaumEl {
+        T data;
+        BaumEl links;
+        BaumEl rechts;
+
+        BaumEl(T content) {
+            data = content;
+            links = null;
+            rechts = null;
+        }
+    }
+
+    public Binaerbaum() {
+        wurzel = null;
+    }
+
+    public Binaerbaum(T e, Binaerbaum links, Binaerbaum rechts) {
+        wurzel = new BaumEl(e);
+        if (rechts != null) {
+            wurzel.rechts = rechts.wurzel;
+        }
+        if (links != null) {
+            wurzel.links = links.wurzel;
+        }
+    }
+
+    public Boolean isEmpty() {
+        return wurzel == null;
+    }
+
+    private int hoeheEl(BaumEl el) {
+        if (el == null) {
+            return 0;
+        }
+        return Math.max(hoeheEl(el.links), hoeheEl(el.rechts)) + 1;
+    }
+
+    public int hoehe() {
+        return hoeheEl(wurzel);
+    }
+
+    public int size() {
+        return sizeEl(wurzel);
+    }
+
+    private int sizeEl(BaumEl el) {
+        if (el == null) {
+            return 0;
+        }
+        return sizeEl(el.links) + sizeEl(el.rechts) + 1;
+    }
+
+    public String toString() {
+        return toStringEl(wurzel);
+    }
+
+    private String toStringEl(BaumEl el) {
+        if (el == null) {
+            return "";
+        }
+        return "(" + toStringEl(el.links) + el.data + toStringEl(el.rechts) + ")";
+    }
+
+    public void preorder(){
+        System.out.println(preorderEl(wurzel));
+    }
+
+    private String preorderEl(BaumEl el){
+        if(el==null){
+            return"";
+        }
+        return(el.data+preorderEl(el.links)+preorderEl(el.rechts));
+
+    }
+
+
+    public void inorder(){
+        System.out.println(inorderEl(wurzel));
+    }
+
+    private String inorderEl(BaumEl el){
+        if(el==null){
+            return"";
+        }
+        return(inorderEl(el.links)+el.data+inorderEl(el.rechts));
+
+    }
+
+
+    public void postorder(){
+        System.out.println(postorderEl(wurzel));
+    }
+
+    private String postorderEl(BaumEl el){
+        if(el==null){
+            return"";
+        }
+        return(postorderEl(el.links)+postorderEl(el.rechts)+el.data);
+
+    }
+
+    public void breitensuche(){
+        EVL speicher = new EVL<BaumEl>();
+        speicher.addFirst(wurzel);
+        for(int i=0; i<=size();i++){
+
+        }
+    }
+
+
+
+
 }
--- src/BinaerbaumTest.java
+++ src/BinaerbaumTest.java
@@ -1,4 +1,50 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BinaerbaumTest {
-  
+
+    Binaerbaum<Integer> baum1 = new Binaerbaum<>();
+    Binaerbaum<Integer> baum2 = new Binaerbaum<>();
+    Binaerbaum<Integer> baum3 = new Binaerbaum<>();
+    Binaerbaum<Integer> baum4 = new Binaerbaum<>();
+    Binaerbaum<Integer> baum5 = new Binaerbaum<>();
+
+
+
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hoehe() {
+        baum3 = new Binaerbaum<Integer>(7, null, null);
+        baum4 = new Binaerbaum<Integer>(9, null,null);
+        baum2 = new Binaerbaum<Integer>(5,baum4,null);
+        baum1 = new Binaerbaum<Integer>(3,baum2,baum3);
+        assertEquals(baum1.hoehe(),3);
+    }
+
+    @Test
+    void size() {
+        baum3 = new Binaerbaum<Integer>(7, null, null);
+        baum4 = new Binaerbaum<Integer>(9, null,null);
+        baum2 = new Binaerbaum<Integer>(5,baum4,null);
+        baum1 = new Binaerbaum<Integer>(3,baum2,baum3);
+        assertEquals(baum1.size(),4);
+    }
+
+    @Test
+    void testToString() {
+        baum3 = new Binaerbaum<Integer>(7, null, null);
+        baum4 = new Binaerbaum<Integer>(9, null,null);
+        baum5 = new Binaerbaum<Integer>(11,null,null);
+        baum2 = new Binaerbaum<Integer>(5,baum4,baum5);
+        baum1 = new Binaerbaum<Integer>(3,baum2,baum3);
+        assertEquals(baum1.toString(),"(((9)5(11))3(7))");
+        baum1.preorder();
+        baum1.inorder();
+        baum1.postorder();
+    }
 }
